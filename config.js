62d1f9fc24aad7e18eb6b652ae70e35a3c9099ca
poppytola <poppytola@gmail.com>
2026-01-27 21:43:08 -0500

    Update config.js

--- config.js
+++ config.js
@@ -78,7 +78,7 @@ const CONFIG = {
     music: {
         enabled: true,                     // Music feature is enabled
         autoplay: true,                    // Try to autoplay (note: some browsers may block this)
-        musicUrl: "https://www.youtube.com/watch?v=XA2YEHn-A8Q", // Music streaming URL
+        musicUrl: "https://res.cloudinary.com/dddp0bptc/video/upload/v1769567731/TWICE__Alcohol-Free__MV_iu0su3.mp3", // Music streaming URL
         startText: "🎵 Play Music",        // Button text to start music
         stopText: "🔇 Stop Music",         // Button text to stop music
         volume: 0.5                        // Volume level (0.0 to 1.0)
